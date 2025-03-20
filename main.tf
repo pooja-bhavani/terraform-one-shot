917fe3e0df01d9411b08feecfdd503e662cc115b
Pooja bhavani <147735975+pooja-bhavani@users.noreply.github.com>
2025-03-20 22:02:11 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -37,6 +37,7 @@ resource "aws_instance" "my_instance" {
     security_groups = [aws_security_group.my_security_group.name]  
     instance_type = var.ec2_instance_type
     ami = var.ec2_ami_id
+    user_data = file("install_nginx.sh")
 
     root_block_device {
         volume_size = var.ec2_root_storage_size
