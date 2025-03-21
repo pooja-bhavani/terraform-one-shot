032af9283483357b0b19b689fc39ab36895bfc9b
Pooja bhavani <147735975+pooja-bhavani@users.noreply.github.com>
2025-03-21 14:53:12 +0530

    Update conditional-expressions.tf

--- conditional-expressions.tf
+++ conditional-expressions.tf
@@ -4,5 +4,5 @@
     ami = var.ec2_ami_id
     user_data = file("install_nginx.sh")
     root_block_device {
-        volume_size = var.env== "prod" ? 20 : var.ec2_default_root_storage_size
+        volume_size = var.env== "prod" ? 20 : var.ec2_default_root_storage_size # if the env is prod then true_value (volume size= 20) if env is not prod then false_value 
         volume_type = "gp3"
